b7471acdd9ac40fac4313f36a68ba4b1d5aff5fd
Cory Grant <cory.grant11@gmail.com>
2024-05-06 23:59:51 -0400

    Update pdm_config.h

--- DingoPDM_FW/PDM/Inc/pdm_config.h
+++ DingoPDM_FW/PDM/Inc/pdm_config.h
@@ -18,9 +18,9 @@
 #include "profet.h"
 #include "wipers.h"
 
-#define PDM_MAJOR_VERSION 7
-#define PDM_MINOR_VERSION 0
-#define PDM_BUILD 1
+#define PDM_MAJOR_VERSION 0
+#define PDM_MINOR_VERSION 1
+#define PDM_BUILD 0
 
 #define PDM_NUM_OUTPUTS 8
 #define PDM_NUM_INPUTS 2
