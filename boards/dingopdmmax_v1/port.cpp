e41f71c924f47e01fd508c38f1d0ac09995dc3f2
Cory Grant <cory.grant11@gmail.com>
2025-09-13 21:48:59 -0400

    typo in max can config, all speed at 500k

--- boards/dingopdmmax_v1/port.cpp
+++ boards/dingopdmmax_v1/port.cpp
@@ -2,39 +2,39 @@
 #include "mcu_utils.h"
 
 static const CANConfig canConfig1000 =
-    {
-        CAN_MCR_ABOM | CAN_MCR_AWUM | CAN_MCR_TXFP,
-        /*
-         For 36MHz http://www.bittiming.can-wiki.info/ gives us Pre-scaler=4, Seq 1=15 and Seq 2=2. Subtract '1' for register values
-        */
-        CAN_BTR_SJW(0) | CAN_BTR_BRP(3) | CAN_BTR_TS1(14) | CAN_BTR_TS2(1),
+{
+    CAN_MCR_ABOM | CAN_MCR_AWUM | CAN_MCR_TXFP,
+    /*
+     For 36MHz http://www.bittiming.can-wiki.info/ gives us Pre-scaler=2, Seq 1=15 and Seq 2=2. Subtract '1' for register values
+    */
+    CAN_BTR_SJW(0) | CAN_BTR_BRP(1)  | CAN_BTR_TS1(14) | CAN_BTR_TS2(1),
 };
 
 static const CANConfig canConfig500 =
-    {
-        CAN_MCR_ABOM | CAN_MCR_AWUM | CAN_MCR_TXFP,
-        /*
-         For 36MHz http://www.bittiming.can-wiki.info/ gives us Pre-scaler=4, Seq 1=15 and Seq 2=2. Subtract '1' for register values
-        */
-        CAN_BTR_SJW(0) | CAN_BTR_BRP(3) | CAN_BTR_TS1(14) | CAN_BTR_TS2(1),
+{
+    CAN_MCR_ABOM | CAN_MCR_AWUM | CAN_MCR_TXFP,
+    /*
+     For 36MHz http://www.bittiming.can-wiki.info/ gives us Pre-scaler=4, Seq 1=15 and Seq 2=2. Subtract '1' for register values
+    */
+    CAN_BTR_SJW(0) | CAN_BTR_BRP(3)  | CAN_BTR_TS1(14) | CAN_BTR_TS2(1),
 };
 
 static const CANConfig canConfig250 =
-    {
-        CAN_MCR_ABOM | CAN_MCR_AWUM | CAN_MCR_TXFP,
-        /*
-         For 36MHz http://www.bittiming.can-wiki.info/ gives us Pre-scaler=4, Seq 1=15 and Seq 2=2. Subtract '1' for register values
-        */
-        CAN_BTR_SJW(0) | CAN_BTR_BRP(3) | CAN_BTR_TS1(14) | CAN_BTR_TS2(1),
+{
+    CAN_MCR_ABOM | CAN_MCR_AWUM | CAN_MCR_TXFP,
+    /*
+     For 36MHz http://www.bittiming.can-wiki.info/ gives us Pre-scaler=8, Seq 1=15 and Seq 2=2. Subtract '1' for register values
+    */
+    CAN_BTR_SJW(0) | CAN_BTR_BRP(7)  | CAN_BTR_TS1(14) | CAN_BTR_TS2(1),
 };
 
 static const CANConfig canConfig125 =
-    {
-        CAN_MCR_ABOM | CAN_MCR_AWUM | CAN_MCR_TXFP,
-        /*
-         For 36MHz http://www.bittiming.can-wiki.info/ gives us Pre-scaler=4, Seq 1=15 and Seq 2=2. Subtract '1' for register values
-        */
-        CAN_BTR_SJW(0) | CAN_BTR_BRP(3) | CAN_BTR_TS1(14) | CAN_BTR_TS2(1),
+{
+    CAN_MCR_ABOM | CAN_MCR_AWUM | CAN_MCR_TXFP,
+    /*
+     For 36MHz http://www.bittiming.can-wiki.info/ gives us Pre-scaler=16, Seq 1=15 and Seq 2=2. Subtract '1' for register values
+    */
+    CAN_BTR_SJW(0) | CAN_BTR_BRP(15)  | CAN_BTR_TS1(14) | CAN_BTR_TS2(1),
 };
 
 const CANConfig &GetCanConfig(CanBitrate bitrate)
